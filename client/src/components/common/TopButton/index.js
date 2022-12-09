b376e6b997e63809d47d55984dbf039aff559921
Collaboration <shjeonlab@gmail.com>
2022-12-09 18:08:20 +0900

    design: 탑(Top) 버튼 추가

---
+++ client/src/components/common/TopButton/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import styles from './style.module.css';
+
+const Index = () => {
+
+    const [showButton, setShowButton] = useState(false);
+
+    const scrollToTop = () => {
+        window.scroll({
+            top: 0,
+            behavior: 'smooth'
+        })
+
+    }
+    useEffect(() => {
+        const ShowButtonClick = () => {
+            if (window.scrollY > 550) {
+                setShowButton(true)
+            } else {
+                setShowButton(false)
+            }
+        }
+        window.addEventListener("scroll", ShowButtonClick)
+        return () => {
+            window.removeEventListener("scroll", ShowButtonClick)
+        }
+    }, [])
+
+    return (
+        <>
+            <button
+                className={showButton && showButton ? `${styles.topBtn_active} ` : `${styles.topBtn}`}  // 버튼 노출 여부
+                onClick={scrollToTop}
+            >
+                <img src="/images/ico_topbutton.png" alt="브라우저 상단으로 이동하기" />
+            </button>
+        </>
+    )
+}
+export default Index
--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import styles from './style.module.css';
 import MainSlider from '../../components/Landing/Slider/MainSlider';
 import SubSlider from '../../components/Landing/Slider/SubSlider';
+import TopButton from '../../components/common/TopButton';
+
 
 const Index = () => {
     return (
         <>
             <MainSlider />
             <SubSlider />
+            <TopButton />
         </>
     )
 }
